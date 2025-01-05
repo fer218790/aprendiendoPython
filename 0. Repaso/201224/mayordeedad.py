395dbb26e626f7642a2f2b5c4906bf35e34759d3
fer218790 <ldonayreq@gmail.com>
2025-01-05 13:11:43 -0500

    commit 05/01/25 - 13:11

---
+++ 0. Repaso/201224/mayordeedad.py
@@ -0,0 +1,6 @@
+#escriba un programa que permita el ingreso de un numero entero que simbolice la edad de una persona.
+#Muestre a continuacion solo si es mayor de edad
+
+edad = int(input("Edad: "))
+if edad>=18:
+    print("Es mayor de edad.")
